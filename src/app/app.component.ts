fe9fc10173084494ba0d2a8eded562466163bb58
Sytnik Oleg <olegsitnik2011@gmail.com>
2021-01-07 19:17:04 +0200

    migrated to angular 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,33 +2,20 @@ import {
     AfterViewInit,
     Component,
     OnInit,
-    ViewChild,
     ViewEncapsulation
 } from '@angular/core';
-import { MatDrawer } from '@angular/material/sidenav';
-import { AppStorageService } from './services/appStorage';
      
 @Component({
     selector: 'app-root',
     templateUrl: 'app.template.html',
-    // styleUrls: ['app.style.scss'],
+    styleUrls: ['app.style.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit, AfterViewInit {
-
-    public isLoading: boolean = false;
-
-    @ViewChild(MatDrawer, {static: true}) private matDrawer: MatDrawer;
     
-    constructor(
-        private AS: AppStorageService
-    ) {}
+    constructor() {}
 
-    ngOnInit(): void {        
-        this.AS.isDataReady$.subscribe(() => {
-            this.isLoading = true;
-        });
-    }
+    ngOnInit(): void {}
 
     ngAfterViewInit(): void {}
 }
